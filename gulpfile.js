87d738d940f260f2461515a97830d9faef7c0334
Randy Waldrop <ranwaldo@yahoo.com>
2015-10-23 15:31:50 -0400

    Install gulp and gulp-util

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+gulp.task('log', function() {                          // create a log 'task'
+  gutil.log('Workflows are awesome');
+});

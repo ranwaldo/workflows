ab4737cb5d543124b6ed3972a6b2c4e3a9917c86
Randy Waldrop <ranwaldo@yahoo.com>
2015-10-26 10:34:11 -0400

    Add environment variables

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,35 @@ var compass = require('gulp-compass');
 var connect = require('gulp-connect');
 var concat = require('gulp-concat');
 
-var coffeeSources = [                                     // all coffee sources to be processed
+var env, coffeeSources, jsSources, sassSources,           // declare variables
+    htmlSources, jsonSources, outputDir, sassStyle;
+
+var env = process.env.NODE_ENV || 'development';          // get env var NODE.ENV
+if (env === 'development') {
+  outputDir = 'builds/development/';
+  sassStyle = 'expanded';
+} else {
+  outputDir = 'builds/production/';
+  sassStyle = 'compressed';
+}
+
+coffeeSources = [                                         // all coffee sources to be processed
   'components/coffee/tagline.coffee'
 ];
-var jsSources = [                                         // all js files to be combined into 1 file
+jsSources = [                                             // all js files to be combined into 1 file
   'components/scripts/rclick.js',
   'components/scripts/pixgrid.js',
   'components/scripts/tagline.js',
   'components/scripts/template.js'
 ];
-var sassSources = [                                       // all sass sources
+sassSources = [                                           // all sass sources
   'components/sass/style.scss'
 ];
-var htmlSources = [                                       // all html sources
-  'builds/development/*.html'
+htmlSources = [                                           // all html sources
+  outputDir + '/*.html'
 ];
-var jsonSources = [                                       // all json sources
-  'builds/development/js/*.json'
+jsonSources = [                                           // all json sources
+  outputDir + '/js/*.json'
 ];
 
 gulp.task('coffee', function() {                          // create task 'coffee'
@@ -35,19 +47,19 @@ gulp.task('js', function() {                              // create task 'js'
   gulp.src(jsSources)                                     // the sources
     .pipe(concat('script.js'))                            // pipe to concat, with output filename
     .pipe(browserify())                                   // pipe to browserify
-    .pipe(gulp.dest('builds/development/js'))             // pipe to dest dir
+    .pipe(gulp.dest(outputDir + '/js'))                   // pipe to dest dir
     .pipe(connect.reload())                               // add pipe to reload server
 });
 gulp.task('compass', function() {                         // create task 'compass'
   gulp.src(sassSources)                                   // input, the sources
     .pipe(compass({                                       // pipe to gulp-compass (options could be in config.rb or here like this)
       sass: 'components/sass',                            // input dir
-      image: 'builds/development/images',                 // images dir
-      style: 'expanded'                                   // more human readable
+      image: outputDir + '/images',                       // images dir
+      style: sassStyle                                    // more human readable
       })
      )
     .on('error', gutil.log)                               // handle error, log it
-    .pipe(gulp.dest('builds/development/css'))            // pipe to output dir
+    .pipe(gulp.dest(outputDir + '/css'))                  // pipe to output dir
     .pipe(connect.reload())                               // add pipe to reload server
 });
 gulp.task('watch', function() {                           // create task 'watch'
@@ -55,7 +67,7 @@ gulp.task('watch', function() {                           // create task 'watch'
   gulp.watch(jsSources, ['js']);                          // when any js file changes, run task 'js'
   gulp.watch('components/sass/*.scss', ['compass']);      // when any sass file changes, run task 'compass'
   gulp.watch(htmlSources, ['html']);                      // when any html file changes, run task 'html'
-  gulp.watch(jsonSources, ['json']);   // when any json file changes, run task 'json'
+  gulp.watch(jsonSources, ['json']);                      // when any json file changes, run task 'json'
 });
 gulp.task('html', function() {                            // create task 'html'
   gulp.src(htmlSources)                                   // the sources
@@ -70,7 +82,7 @@ gulp.task('default', ['html','json','coffee','js','compass','connect','watch']);
 
 gulp.task('connect', function() {                         // create task 'connect'
   connect.server({                                        // call server()
-    root: 'builds/development/',                          // document root
+    root: outputDir,                                      // document root
     port: 9000,                                           // set port
     livereload: true                                      // do live reloads
   });
